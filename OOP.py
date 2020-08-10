469469b0066991a449a1b22ad34f013e5307d906
Akhmad <khalilovaha@gmail.com>
2020-08-10 20:45:56 +0300

    changed

--- OOP.py
+++ OOP.py
@@ -258,3 +258,38 @@ class Carnivour2(Animal2):
 cat = Carnivour2()
 cat.hit(15)
 print(cat.health)
+
+#миксины - особенный вид множественного наследования, не создаются как базовый класс
+
+class Vehicle:
+    def __init__(self, position):
+        self.position = position
+    def trevel(self, destination):
+        route = calculate_route(source=self.position, to = destination)
+        self.move_along(route)
+    def calculate_route(self, source, to):
+        return 0
+    def move_along(self, route):
+        print("moving")
+
+class Airplane(Vehicle):
+    pass
+#поключим радио для машины, но чтобы оно не подключилось к самолете через миксин
+class RadioMixin:
+    def __init__(self):
+        self.radio = Radio()
+    def turn_on(self, station):
+        self.radio.set_station(station)
+        self.radio.play()
+class Radio:
+    def set_station(self, station):
+        self.station = station
+    def play(self):
+        print(f"Playing {self.station}")
+
+class Car(Vehicle, RadioMixin):
+    def __init__(self):
+        Vehicle.__init__(self, (10, 20))
+        RadioMixin.__init__(self)
+car = Car()
+car.turn_on("Moscow FM")
